1f5e774a41f94aea31f54332a9593e403cdaf63c
Andreyeyeye <shebanoff.andrei@yandex.ru>
2024-10-09 04:19:15 +0700

    добавил почти полностью рабочий интерфейс и доделал интерфейс для работы из файла

--- tyap2/MainWindow.xaml.cs
+++ tyap2/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace tyap2
 {
     /// <summary>
@@ -22,15 +23,140 @@ namespace tyap2
     public partial class MainWindow : Window
     {
 
+
+
+
+        int StatesNum = 0;
+        int AlphabetLength = 0;
+        string[] AlphabetArray = new string[0];
+        private Dictionary<TextBox, TextBox> RulesDict = new Dictionary<TextBox, TextBox>();
+        bool s1 = false;
+        bool s2 = false;
+        List<string> NonTerminalSymb = new List<string>();
+        List<string> TerminalSymb = new List<string>();
+        List<string> AllString = new List<string>();
+
+
         private Dictionary<string, string> RulesDictStrings = new Dictionary<string, string>();
         private string SpecialSymbol = "_";
-        private string Chain = "0000001";
+        private string SpecialSymbolSolDelim = "\n";
+        private string Chain = "";
         private string StartStage = "";
         private string FinalStage = "";
 
         public MainWindow()
         {
             InitializeComponent();
+ /*           string fileStatus = ReadFromFile("l.txt");
+            Console.WriteLine(fileStatus);
+            if (fileStatus == "ok")
+            {
+                //файл считан успешно
+
+                Console.WriteLine(ChainCheck());
+
+
+            }
+            */
+
+        }
+
+
+        private void CreateTextBoxes()
+        {
+            for (int i = 0; i < StatesNum; i++)
+            {
+                for(int j = 0; j < AlphabetLength; j++)
+                {
+
+                    var stackPanel = new StackPanel { Orientation = Orientation.Horizontal };
+
+                    var textBox1 = new TextBlock { Width = 30, Name = $"D{i + 1}1"};
+                    Console.WriteLine(AlphabetArray[j]);
+                    textBox1.Text = i.ToString();
+                    var textBox2 = new TextBox { Width = 30, Name = $"D{i + 1}1", MaxLength = 1 };
+                    textBox2.Text = AlphabetArray[j];
+                    var label1 = new Label { Content = "   " };
+                    var label2 = new Label { Content = " -> " };
+                    var textBox3 = new TextBox { Width = 30, Name = $"D{i + 1}2" };
+
+                    stackPanel.Children.Add(textBox1);
+                    stackPanel.Children.Add(label1);
+                    stackPanel.Children.Add(textBox2);
+                    stackPanel.Children.Add(label2);
+                    stackPanel.Children.Add(textBox3);
+
+                    MainStackPanel.Children.Add(stackPanel);
+
+                    // Добавляем текстовые поля в словарь
+                    RulesDict.Add(textBox2, textBox3);
+
+                }
+
+
+            }
+        }
+        private void SettingsApply_Click(object sender, RoutedEventArgs e)
+        {
+            if (s1)
+            {
+                return;
+            }
+            if (StatesNumInput.Text == "" || AlphabetSymbolsInput.Text == "")
+            {
+                ErrorTextbox.Text = "пустые строки";
+                return;
+            }
+            if (Int32.TryParse(StatesNumInput.Text, out StatesNum))
+            {
+
+            }
+            else
+            {
+                ErrorTextbox.Text = "введено не число в число состояний";
+                return;
+            }
+            /*            if (Int32.TryParse(AlphabetLengthInput.Text, out AlphabetLength))
+                        {
+
+                        }
+                        else
+                        {
+                            ErrorTextbox.Text = "введено не число в длину алфавита";
+                            return;
+                        }*/
+
+            if (AlphabetSymbolsInput.Text.Length == 0)
+            {
+                ErrorTextbox.Text = "пустой словарь";
+                return;
+            }
+
+            AlphabetArray = AlphabetSymbolsInput.Text.Split(' ');
+
+            AlphabetLength = AlphabetArray.Length;
+
+            Console.WriteLine(StatesNum + " " + AlphabetSymbolsInput);
+            CreateTextBoxes();
+            s1 = true;
+        }
+
+        private void ManualInputButton_Click(object sender, RoutedEventArgs e)
+        {
+            ST1.Height = 0;
+            ST2.Height = 30;
+            ST3.Height = 30;
+            //вывести на экран интерфейс для добавления правил
+        }
+
+
+
+        private void FileInputButton_Click(object sender, RoutedEventArgs e)
+        {
+            ST1.Height = 0;
+            ST3.Height = 30;
+            //вызвать функции для считывания из файла и выведения на экран
+
             string fileStatus = ReadFromFile("l.txt");
             Console.WriteLine(fileStatus);
             if (fileStatus == "ok")
@@ -41,12 +167,43 @@ namespace tyap2
 
 
             }
-            
+
 
         }
 
+        private void CalcButton_Click(object sender, RoutedEventArgs e)
+        {
 
-        private string ReadFromFile(string filename)
+
+            if (ST2.Height == 0)
+            {
+
+            } else
+            {
+                // берутся значения из инпутов
+            }
+
+
+
+
+
+            Chain = CheckStringInput.Text;
+
+
+            string outputString = ChainCheck();
+            var labelVar = new TextBlock { Width = 800 };
+            labelVar.Text = outputString;
+            ScrollViewer scrollViewer = new ScrollViewer
+            {
+                VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+                Content = labelVar,
+                Height = 400
+            };
+            MainStackPanel.Children.Add(scrollViewer);
+
+        }
+
+            private string ReadFromFile(string filename)
         {
 
             string[] lines; 
@@ -118,7 +275,7 @@ namespace tyap2
                 {
 
 
-                    changes += " -> ";
+                    changes += SpecialSymbolSolDelim;
                 } else
                 {
                     changes += " ";
@@ -137,7 +294,7 @@ namespace tyap2
             }
             if (stage != FinalStage)
             {
-                changes += " -> " +  stage + " автомат не в финальном состоянии, должно быть " + FinalStage;
+                changes += SpecialSymbolSolDelim +  stage + " автомат не в финальном состоянии, должно быть " + FinalStage;
                 return changes;
             }
             else
